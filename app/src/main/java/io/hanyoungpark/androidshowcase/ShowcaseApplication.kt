776a920c1ac296a06a4a6c7916ecbc210b092312
Hanyoung Park <hanyoungpark@gmail.com>
2021-08-25 21:48:51 -0700

    Setup Hilt

---
+++ app/src/main/java/io/hanyoungpark/androidshowcase/ShowcaseApplication.kt
@@ -0,0 +1,8 @@
+package io.hanyoungpark.androidshowcase
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShowcaseApplication: Application() {
+}
