c02fb0b47641bedb272ba625a4d94aa42c5c0671
Hanyoung Park <hanyoungpark@gmail.com>
2021-08-30 21:48:21 -0700

    Implement basic viewmodel without pager (#4)

---
+++ app/src/main/java/io/hanyoungpark/androidshowcase/viewmodels/GiphyViewModel.kt
@@ -0,0 +1,34 @@
+package io.hanyoungpark.androidshowcase.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.hanyoungpark.androidshowcase.models.DataModel
+import io.hanyoungpark.androidshowcase.repositories.GiphyRepository
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GiphyViewModel @Inject constructor(
+    private val giphyRepository: GiphyRepository
+) : ViewModel() {
+
+    private val _searchResult = MutableLiveData<List<DataModel>>()
+    val searchResult: LiveData<List<DataModel>>
+        get() = _searchResult
+
+    private val page = 0
+    private val size = 50
+
+    fun search(query: String) {
+        viewModelScope.launch {
+            val flow = giphyRepository.search(query, size, page)
+            flow.collect {
+                _searchResult.postValue(it.data)
+            }
+        }
+    }
+}
